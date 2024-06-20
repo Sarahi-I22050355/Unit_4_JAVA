b7f207401ea43259a227bc53eec0858b136663cd
Sarah <Sarah@HP-SARAHI>
2024-06-20 04:24:11 -0600

    Unit 4 JAVA

---
+++ src/unit_4_java/Unit_4_JAVA.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package unit_4_java;
+import Forms.App;
+
+/**
+ *
+ * @author Sarah
+ */
+public class Unit_4_JAVA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        App miFormulario = new App();
+        miFormulario.setVisible(true);
+    }
+    
+}
